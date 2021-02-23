1922888fd977c006511ebdcd20af41cb81436a7b
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-23 19:02:57 +0530

    While loop used

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,26 @@
 isFulltime=0;
 isParttime=1;
 ratePerhour=20;
-randomCheck=$(( $RANDOM % 3 ));
-case $randomCheck in
-    $isFulltime)hoursWorked=8
+workingDays=1;
+maxDays=20;
+while (( $workingDays<=maxDays ))
+do
+    employeeCheck=$(($RANDOM%3));
+      case $employeeCheck in
+         $isFulltime)
+                hoursWorked=8
                 ;;
-    $isParttime)hoursWorked=4
+         $isParttime)
+                hoursWorked=4
                 ;;
-    *)hoursWorked=0
+         *)
+                hoursWorked=0
                 ;;
 esac
 
 salary=$(($hoursWorked * $ratePerhour));
+monthlySalary=$(($totalSalary+$salary));
+((workingDays++));
+done
+
+echo $monthlySalary
