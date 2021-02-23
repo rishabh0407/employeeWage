cc4806f83a5d1438d4eee24d8bc660d75fc9b1a1
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-23 19:18:38 +0530

    Altered:-function for workingHours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -x
 
+#Constants
 isFulltime=0;
 isParttime=1;
 ratePerhour=20;
 workingDays=1;
 maxDays=20;
-maxHours=8;
+maxHours=100;
+
+#Variables
+totalHours=0;
+workingDays=0;
 totalHoursWorked=0;
-while (( $workingDays<=maxDays && $totalHoursWorked<=maxHours))
-do
-    employeeCheck=$(( $RANDOM%3 ));
-      case $employeeCheck in
+
+declare -A empDailyWage
+function hours()
+{
+case $1 in
          $isFulltime)
                 hoursWorked=8
                 ;;
@@ -20,12 +26,17 @@ do
          *)
                 hoursWorked=0
                 ;;
-esac
+        esac
+        echo $hoursWorked
 
-salary=$(($hoursWorked * $ratePerhour));
-monthlySalary=$(($totalSalary+$salary));
-totalHoursWorked=$(($totalHoursWorked+$hoursWorked));
-((workingDays++));
+}
+
+while (( $workingDays<=maxDays && $totalHoursWorked<=$maxHours ))
+do
+         hoursWorkedFinal="$( hours $(( RANDOM % 3 )) )"
+         totalHoursWorked=$(($totalHoursWorked+$hoursWorkedFinal));
+         salary=$(($hoursWorkedFinal * $ratePerhour));
+         monthlySalary=$(($monthlySalary+$salary));
+         ((workingDays++));
 done
-
-echo $monthlySalary
+echo "Monthly Salary=" $monthlySalary
