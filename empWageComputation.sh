6e9cc0903ee167df23b9b260863515a24f3c6bcd
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-23 18:36:28 +0530

    Altered the code :-UC-2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,9 +2,14 @@
 
 random=$(( RANDOM % 2 ))
 ispresent=0;
+wagePerHour=20;
+hoursWorked=8;
 if(( $random == $ispresent ));
 then 
+     salary=$(( $wagePerHour * $hoursWorked ));
      echo "Present"
+     echo  "Salary=" $salary
 else
      echo "Absent"
+     echo "Salary=0"
 fi
