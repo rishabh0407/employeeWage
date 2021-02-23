42876879ee43b1cc8da3ff898cb2894ed61abfdf
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-23 18:51:29 +0530

    Altered the code again

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -x
 
-echo "Welcome to Employee"
-echo "Wage Computation"
-echo "Program on Master Branch"
+isFulltime=0;
+isParttime=1;
+randomCheck=$(( $RANDOM % 3 ));
+if(( $randomCheck==$isFulltime ))
+then
+   hoursworked=8;
+   rateperhour=100;
+   salary=$(( $hoursworked * $rateperhour ))
+   echo $salary
+elif(( $randomCheck==$isParttime ))
+then
+   hoursworked=4;
+   rateperhour=100;
+   salary=$(( $hoursworked * $rateperhour ))
+   echo $salary
+else
+   echo "Absent"
+   echo "Salary=0"
+fi
+
+
